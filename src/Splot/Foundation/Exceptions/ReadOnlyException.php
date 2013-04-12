0edce5ec80e70a039fdb2417ee58c59e68d059d2
Michał Dudek <michal@michaldudek.pl>
2013-04-12 23:36:27 +0200

    Added ReadOnlyException.

---
+++ src/Splot/Foundation/Exceptions/ReadOnlyException.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Exception usually thrown when trying to overwrite something that is read only.
+ * 
+ * @package SplotFoundation
+ * @subpackage Exceptions
+ * @author Michał Dudek <michal@michaldudek.pl>
+ * 
+ * @copyright Copyright (c) 2013, Michał Dudek
+ * @license MIT
+ */
+namespace Splot\Foundation\Exceptions;
+
+use Splot\Foundation\Exceptions\Exception;
+
+class ReadOnlyException extends Exception
+{
+
+
+
+}
